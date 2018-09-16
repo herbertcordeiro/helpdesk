98e30a6a21da8f6cfc79eaed9b74caa6a71affd5
KalberRoberto <kalber_roberto@hotmail.com>
2018-09-16 18:07:18 -0300

    criacao model categoria

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Ticket
+from .models import Categoria
 
 admin.site.register(Ticket)
+admin.site.register(Categoria)
--- ticket/models.py
+++ ticket/models.py
@@ -1,19 +1,13 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome=models.CharField(max_length=100)
+
 class Ticket(models.Model):
-    banheiro = '1'
-    sala = '2'
-    labs = '3' 
-    
-    CATEGORIAS = (
-        (banheiro, 'BANHEIRO'),
-        (sala, 'SALA'),
-        (labs, 'LABS'),
-    )
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     nome_completo = models.CharField(max_length=50)
     email = models.EmailField()
     telefone = models.CharField(max_length=14)
-    categoria = models.CharField(max_length=1, choices=CATEGORIAS)
     anexo = models.FileField(upload_to='post_image', blank=True)
     descricao = models.TextField()
 
