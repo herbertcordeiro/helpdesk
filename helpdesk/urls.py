328a479ca7dec703b99f57138bbc02f024f0f4bf
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-09-18 14:33:27 -0300

    login

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('home', index, name='home'),
     path('tickets/', include('ticket.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import TicketForm
 from .models import Ticket
+from django.contrib.auth.decorators import login_required
 
 def newticket(request):
     form = TicketForm(request.POST, request.FILES, None)
