1061a41900756549542f5e6c9c887b9527f2a295
KalberRoberto <kalber_roberto@hotmail.com>
2018-09-20 20:57:51 -0300

    criar APP home, pg de sair

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index
 from django.contrib.auth import views as auth_views
+from home import urls as home_urls
+
 
 
 urlpatterns = [
+    path('', include(home_urls)),
     path('home', index, name='home'),
     path('tickets/', include('ticket.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
 ]
---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home
+from .views import myLogout
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('logout/', myLogout, name='logout'),
+
+]
