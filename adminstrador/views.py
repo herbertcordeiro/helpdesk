52f793ebb7cacf3b1a227b840cb211adeb1831bc
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-09-25 01:59:26 -0300

    listar ticket

--- adminstrador/views.py
+++ adminstrador/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from ticket.models import Ticket
 
-@login_required
 def inicial(request):
-    return render(request, 'listartickets.html')
+    tickets = Ticket.objects.all()
+    return render(request, 'listartickets.html', {'tickets': tickets})
