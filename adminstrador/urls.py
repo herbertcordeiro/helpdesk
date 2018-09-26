0eba0fc5b60e5a64e0b94246accb3a07763a2372
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-09-25 23:34:50 -0300

    back listar

--- adminstrador/urls.py
+++ adminstrador/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import inicial
-from .views import dash
+from .views import pesquisa_id_nome
 
 urlpatterns = [
     path('inicial/', inicial, name='inicial'),
+    path('pesquisa/', pesquisa_id_nome, name='pesquisa'),
 ]
--- adminstrador/views.py
+++ adminstrador/views.py
@@ -4,5 +4,21 @@ from ticket.models import Ticket
 
 @login_required
 def inicial(request):
-    tickets = Ticket.objects.all()
+    filter = request.GET.get("filter", None)
+    if(filter != None):
+        tickets = Ticket.objects.filter(status=filter)
+    else:
+        tickets = Ticket.objects.all()
     return render(request, 'listartickets.html', {"tickets": tickets})
+
+
+@login_required
+def pesquisa_id_nome(request):
+    filter = request.GET.get("id", None)
+    
+    if(filter != None):
+        tickets2 = Ticket.objects.filter(id=filter)
+    else:
+        tickets2 = Ticket.objects.all()
+        
+    return render(request, 'listartickets.html', {"tickets": tickets2})
