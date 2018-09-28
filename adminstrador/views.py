fcd87e8bde4799a967d68e66880ea4606f46e1ea
herbertcordeiro <herbert.cordeiro@dce.ufpb.br>
2018-09-28 02:02:10 -0300

    pagination

--- adminstrador/views.py
+++ adminstrador/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from ticket.models import Ticket
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 @login_required
 def inicial(request):
-    filter = request.GET.get("filter", None)
-    if(filter != None):
-        tickets = Ticket.objects.filter(status=filter)
-    else:
-        tickets = Ticket.objects.all()
+    tickets_list = Ticket.objects.all()
+    paginator = Paginator(tickets_list, 5)
+    page = request.GET.get('page')
+    tickets = paginator.get_page(page)
     return render(request, 'listartickets.html', {"tickets": tickets})
 
 
