f8acd030e6f22839527152f43a1558355ed61dd1
herbertcordeiro <herbert.cordeiro@dce.ufpb.br>
2018-09-28 04:26:50 -0300

    ajustes

--- adminstrador/urls.py
+++ adminstrador/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import inicial
+from .views import details
 from .views import pesquisa_id
 
 urlpatterns = [
     path('inicial/', inicial, name='inicial'),
+    path('details/<int:id>', details, name='details'),
     path('pesquisa/', pesquisa_id, name='pesquisa'),
 ]
--- adminstrador/views.py
+++ adminstrador/views.py
@@ -6,18 +6,14 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 @login_required
 def inicial(request):
-    
     filter = request.GET.get("filter", None)
-   
     if(filter != None):
         tickets_list = Ticket.objects.filter(status=filter)
     else:
         tickets_list = Ticket.objects.order_by("status").all()
-   
     paginator = Paginator(tickets_list, 5)
     page = request.GET.get('page')
-    tickets = paginator.get_page(page)
-   
+    tickets = paginator.get_page(page) 
     return render(request, 'listartickets.html', {"tickets": tickets, 'filter': filter, 'page': page })
 
 
@@ -30,3 +26,7 @@ def pesquisa_id(request):
         tickets2 = Ticket.objects.all()
     return render(request, 'listartickets.html', {"tickets": tickets2})
 
+@login_required
+def details(request, id):
+    ticket = Ticket.objects.get(id = id)
+    return render(request, 'detailsticket.html', {'ticket': ticket})
