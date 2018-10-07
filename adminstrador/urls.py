a1a24a532ba04fd12fba309f91c968e33a9ac695
KalberRoberto <kalber_roberto@hotmail.com>
2018-10-07 04:35:26 -0300

    redefinição de senha

--- adminstrador/urls.py
+++ adminstrador/urls.py
@@ -3,6 +3,7 @@ from .views import inicial
 from .views import details
 from .views import pesquisa_id
 
+
 urlpatterns = [
     path('inicial/', inicial, name='inicial'),
     path('details/<int:id>', details, name='details'),
--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -5,6 +5,7 @@ from .views import handler404,handler500
 from django.contrib.auth import views as auth_views
 from home import urls as home_urls
 from adminstrador import urls as adminstrador_urls
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm,password_reset_complete
 
 
 urlpatterns = [
@@ -13,7 +14,15 @@ urlpatterns = [
     path('tickets/', include('ticket.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.login, name='login'),
+    path('esqueceu-senha/', password_reset, name='esqueceu-senha'),
+    path('esqueceu-senha/concluido', password_reset_done, name='password_reset_done'),
+    path('esqueceu-senha/confirmacao/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', password_reset_confirm, name='password_reset_confirm'),
+     path('esqueceu-senha/completo/', password_reset_complete, name='password_reset_complete'),
+
 ]
 
 handler404 = handler404
 handler500 = handler500
+
+
+"""pra testar o email python -m smtpd -n -c DebuggingServer localhost:1025"""
