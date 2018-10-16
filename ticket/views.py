c21289c84dd563f2e9ac51fadb89b8fd1f92dc08
Guthyelvis Santos <guthyelvis.santos@dce.ufpb.br>
2018-10-16 03:30:17 -0300

    tratamento de erro pesquisar ticket

--- ticket/views.py
+++ ticket/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import TicketForm
 from .models import Ticket
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def newticket(request):
     form = TicketForm(request.POST, request.FILES, None)
@@ -14,11 +15,15 @@ def newticket(request):
 def pesquisa_ticket(id):
     ticket = Ticket.objects.get(id=id)
     return ticket
-
+      
 def mostrar_ticket(request):
-    ticket = pesquisa_ticket(request.GET['id'])
-    context_dict = {'ticket': ticket}
-    return render(request, 'mostrarticket.html', context = context_dict)
-
+    try:
+        ticket = pesquisa_ticket(request.GET['id'])
+        context_dict = {'ticket': ticket}
+        return render(request, 'mostrarticket.html', context = context_dict)
+    except:
+        context_dict = {'ticket': None}
+        messages.error(request, 'Ticket não existe')
+        return render(request, 'searchticket.html',context = context_dict)
 
 
