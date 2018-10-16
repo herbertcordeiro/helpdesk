95c290eb6228087acecf9d1291c167de4145cde7
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-10-16 06:39:42 -0300

    [bug] pesquisar por id

--- adminstrador/views.py
+++ adminstrador/views.py
@@ -7,29 +7,21 @@ from django.shortcuts import redirect
 
 @login_required
 def inicial(request):
-    tickets_list = listaDeTickts(request)
+    if(request.user.is_superuser):
+        tickets_list = Ticket.objects.all()
+    else:
+        tickets_list = Ticket.objects.filter(user=request.user)     
     paginator = Paginator(tickets_list, 5)
     page = request.GET.get('page')
     tickets = paginator.get_page(page) 
     return render(request, 'listartickets.html', {"tickets": tickets, 'filter': filter, 'page': page })
 
-@login_required
-def listaDeTickts(request):
-    tickets = Ticket.objects.all()
-    tickets2 = []
-    if(request.user.is_superuser):
-        return tickets
-    else:
-        for ticket in tickets:
-            if(ticket.user == request.user):
-                tickets2.append(ticket)
-        return tickets2
 
 @login_required
 def pesquisa_id(request):
     filter = request.GET.get("id", None)
     if(filter != None):
-        tickets2 = Ticket.objects.filter(id=filter)
+        tickets2 = Ticket.objects.filter(user=request.user).filter(id=filter)  
     else:
         tickets2 = Ticket.objects.all()
     return render(request, 'listartickets.html', {"tickets": tickets2})
