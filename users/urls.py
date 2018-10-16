6c44e0d80ae735424768cbd42a5d9a4743413887
KalberRoberto <kalber_roberto@hotmail.com>
2018-10-16 04:30:28 -0300

    apagar usuario, correção no cadastrar

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import newUser, users
+from .views import newUser, users, delete_users
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', users, name="users"),
     path('newUser/', newUser, name='newUser'),
+    path('delete/<int:user_id>/', delete_users, name='user_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from .forms import UserForm
 from django.contrib.auth.models import User
 
@@ -6,9 +6,14 @@ def newUser(request):
     form = UserForm(request.POST, request.FILES, None)
     if form.is_valid():
         form.save()
-        print('oxi')
+        return redirect ('users')
     return render(request, 'newUser.html', {'form': form})
     
 def users(request):
     userList = User.objects.values()
     return render(request, 'users.html', {"userList": userList})
+
+def delete_users(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.delete()
+    return HttpResponseRedirect('/users/')
