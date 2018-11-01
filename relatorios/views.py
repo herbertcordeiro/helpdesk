14b643921539572e3cd9e65f96c0aa96a0845668
KalberRoberto <kalber_roberto@hotmail.com>
2018-11-01 02:36:50 -0300

    gerador de PDF

--- relatorios/views.py
+++ relatorios/views.py
@@ -2,11 +2,17 @@ import json
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from ticket.models import Ticket
+from users.views import listar_usuarios
+from users.forms import User
 from adminstrador.views import listartickets
+from django.contrib.auth.models import User
 
 @login_required
 def relatorios(request):
+    nomes=[]
     total=[]
+    tick=[]
+    temp=[]
     total_novo=0
     total_aberto=0
     total_fechado=0
@@ -18,15 +24,33 @@ def relatorios(request):
             total_aberto=total_aberto+1
         else:
             total_fechado=total_fechado+1
+    usuarios=listar_usuarios(request)
+    for i in usuarios:
+        nomes.append(i.username)   
     total.append(total_novo)
     total.append(total_aberto)
     total.append(total_fechado)
-    categoria = [obj.categoria.nome for obj in tickets]
     context = {
         'total': json.dumps(total),
-        'categoria': json.dumps(categoria),
+        'nomes': json.dumps(nomes),
+        'tick': json.dumps(tick),
     }
     return render(request, 'relatorios.html', context)
 
 
+def separar_tickets(request):
+    tickets_abertos=[]
+    tickets_fechados=[]
+    tickets_novo=[]
+    tickets = listartickets(request)
+    usuarios=listar_usuarios(request)
+    for i in tickets:
+        if i.status=='1':
+            tickets_novo.append(i)
+        elif i.status=='2':
+            tickets_abertos.append(i)
+        else:
+            tickets_fechados.append(i)
+    
+    
 
